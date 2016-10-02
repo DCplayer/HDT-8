1c7bb3ec5e28454e2efc8789ec74613c91ca43e7
CoffeeBrewer <cas15151@uvg.edu.gt>
2016-10-02 10:08:31 -0600

    Creacion de la Clase Paciente
    En donde se implementa Comparable<Paciente> para determinar quien de los
    dos pacientes es mas prioritario en cuanto a enfermedad

---
+++ Paciente.java
@@ -0,0 +1,38 @@
+/**
+ * Created by DiegoC on 02/10/2016.
+ */
+public class Paciente implements Comparable<Paciente> {
+    public String nombre;
+    public String diagnostico;
+    public char prioridad;
+
+    public Paciente(String nombre, String diagnostico, char prioridad){
+        this.nombre = nombre;
+        this.diagnostico = diagnostico;
+        this.prioridad = prioridad;
+    }
+    public char getPrioridad() {
+        return prioridad;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public int compareTo(Paciente o) {
+        int propio = (int)this.prioridad;
+        int ajeno = (int)o.getPrioridad();
+        if(propio >= ajeno){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+
+    }
+}
