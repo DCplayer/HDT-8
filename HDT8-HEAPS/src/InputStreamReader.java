83c8e8e19143c3ead254eb4a9c7191dca8c8f6cb
ING MIGUEL MORALES <ING MIGUEL MORALES@192.168.1.5>
2016-10-03 16:22:40 -0600

    PROYECT

---
+++ HDT8-HEAPS/src/InputStreamReader.java
@@ -0,0 +1,9 @@
+import org.omg.CORBA.DataInputStream;
+
+/**
+ * Created by Christian12 on 02/10/2016.
+ */
+public class InputStreamReader {
+    public InputStreamReader(DataInputStream ent) {
+    }
+}
