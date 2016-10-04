8822680a5a0fbc0df961c2f18a43e4e07d09acf9
ChristianFML <mor15015@uvg.edu.gt>
2016-10-03 19:00:53 -0600

    Delete InputStreamReader.java

--- InputStreamReader.java
+++
@@ -1,9 +0,0 @@
-import org.omg.CORBA.DataInputStream;
-
-/**
- * Created by Christian12 on 02/10/2016.
- */
-public class InputStreamReader {
-    public InputStreamReader(DataInputStream ent) {
-    }
-}
